2c8091e5b062d236a2a8a6b7f537e5d7a4b87be1
Charles <chaoocharles@gmail.com>
2024-02-11 13:03:15 +0300

    lightbox next and prev functionality

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ let thumbnails = document.querySelectorAll(".default .thumb-list div");
 let lightboxMainImages = document.querySelectorAll(".lightbox .main-img img");
 let lightboxThumbnails = document.querySelectorAll(".lightbox .thumb-list div");
 let lightbox = document.querySelector(".lightbox");
+let iconClose = document.querySelector(".icon-close");
+let iconPrev = document.querySelector(".icon-prev");
+let iconNext = document.querySelector(".icon-next");
+
+let currentImageIndex = 0;
 
 const changeImage = (index, mainImages, thumbnails) => {
   mainImages.forEach((img) => {
@@ -14,6 +19,7 @@ const changeImage = (index, mainImages, thumbnails) => {
 
   mainImages[index].classList.add("active");
   thumbnails[index].classList.add("active");
+  currentImageIndex = index;
 };
 
 thumbnails.forEach((thumb, index) => {
@@ -33,4 +39,24 @@ mainImages.forEach((img, index) => {
     lightbox.classList.add("active");
     changeImage(index, lightboxMainImages, lightboxThumbnails);
   });
+});
+
+iconPrev.addEventListener("click", () => {
+  if (currentImageIndex <= 0) {
+    changeImage(mainImages.length - 1, lightboxMainImages, lightboxThumbnails);
+  } else {
+    changeImage(currentImageIndex - 1, lightboxMainImages, lightboxThumbnails);
+  }
+});
+
+iconNext.addEventListener("click", () => {
+  if (currentImageIndex >= mainImages.length - 1) {
+    changeImage(0, lightboxMainImages, lightboxThumbnails);
+  } else {
+    changeImage(currentImageIndex + 1, lightboxMainImages, lightboxThumbnails);
+  }
+});
+
+iconClose.addEventListener("click", () => {
+  lightbox.classList.remove("active");
 });
