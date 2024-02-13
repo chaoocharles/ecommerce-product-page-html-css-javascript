0b390bcb45c4bcac2b35301a2f5f59648c756be8
Charles <chaoocharles@gmail.com>
2024-02-13 13:37:44 +0300

    remove item from cart

--- cart.js
+++ cart.js
@@ -40,6 +40,8 @@ const updateTotalCartQty = () => {
   cartCount.innerHTML = `<span class="qty">${totalCartQty}</span>`;
 };
 
+// add item to cart
+
 const addItemToCart = (name, price, imageSrc) => {
   const totalPrice = count * price;
 
@@ -57,7 +59,9 @@ const addItemToCart = (name, price, imageSrc) => {
             </p>
         </div>
         </div>
-        <svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg>
+        <button class="delete-item"> 
+            <svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg>
+        </button>
     `;
 
   cartItems.appendChild(cartItem);
@@ -68,6 +72,14 @@ const addItemToCart = (name, price, imageSrc) => {
     cartItems.classList.remove("empty");
     checkout.classList.remove("empty");
   }
+
+  // attach an event listener to the delete button
+
+  const deleteButton = cartItem.querySelector(".delete-item");
+  deleteButton.addEventListener("click", (event) => {
+    const cartItem = event.target.closest(".cart-item");
+    removeItemFromCart(cartItem);
+  });
 };
 
 addToCartBtn.addEventListener("click", () => {
@@ -80,6 +92,19 @@ addToCartBtn.addEventListener("click", () => {
     .getAttribute("src");
 
   addItemToCart(productName, productPrice, productImg);
+  cartContainer.classList.add("active");
 
   updateCount(0);
 });
+
+// remove item from cart
+
+const removeItemFromCart = (cartItem) => {
+  cartItem.remove();
+  updateTotalCartQty();
+
+  if (!cartItems.classList.contains("empty")) {
+    cartItems.classList.add("empty");
+    checkout.classList.add("empty");
+  }
+};
